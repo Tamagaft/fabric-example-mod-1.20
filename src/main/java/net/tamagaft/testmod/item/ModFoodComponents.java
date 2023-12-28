d2e56e05ec89d135bb28f76ef1833424db1bbdd3
TaMaG <ihorvoloshanovich@gmail.com>
2023-12-28 18:41:51 +0200

    Added food (tomato)

---
+++ src/main/java/net/tamagaft/testmod/item/ModFoodComponents.java
@@ -0,0 +1,11 @@
+package net.tamagaft.testmod.item;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
+
+public class ModFoodComponents {
+    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS,60),0.25f).build();
+}
