49bee68471b13a1bcf51fbfd5cd56390b8dea6df
TaMaG <ihorvoloshanovich@gmail.com>
2023-12-28 21:14:12 +0200

    added tooltips

--- src/main/java/net/tamagaft/testmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/tamagaft/testmod/item/custom/MetalDetectorItem.java
@@ -3,13 +3,19 @@ package net.tamagaft.testmod.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.tamagaft.testmod.block.ModBlocks;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings){
@@ -40,6 +46,12 @@ public class MetalDetectorItem extends Item {
                 playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.testmod.metal_detector.tooltip"));
+        super.appendTooltip(stack,world,tooltip,context);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
