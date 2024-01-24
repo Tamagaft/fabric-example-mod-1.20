45e4890f9da76ef66c75a88811e594a0e8ada28a
Tamagaft <ihorvoloshanovich@gmail.com>
2024-01-24 20:04:38 +0200

    Added armour

--- src/main/java/net/tamagaft/testmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/tamagaft/testmod/datagen/ModItemTagProvider.java
@@ -3,6 +3,8 @@ package net.tamagaft.testmod.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+import net.tamagaft.testmod.item.ModItems;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -12,6 +14,7 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
     }
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
-
+        getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.RUBY_HELMET,ModItems.RUBY_LEGGINGS,ModItems.RUBY_CHESTPLATE,ModItems.RUBY_BOOTS);
     }
 }
