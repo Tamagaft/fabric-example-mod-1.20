6bbd7eb6507e4d5d2ca24c249e13838ca1cb3389
Tamagaft <ihorvoloshanovich@gmail.com>
2024-01-24 21:52:12 +0200

    Modifying loot tables of suspicious sand example

--- src/main/java/net/tamagaft/testmod/util/ModLootTablesModifiers.java
+++ src/main/java/net/tamagaft/testmod/util/ModLootTablesModifiers.java
@@ -1,9 +1,12 @@
 package net.tamagaft.testmod.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
@@ -11,11 +14,17 @@ import net.minecraft.util.Identifier;
 import net.tamagaft.testmod.block.ModBlocks;
 import net.tamagaft.testmod.item.ModItems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ModLootTablesModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID = new Identifier("minecraft","chests/jungle_temple");
     private static final Identifier CREEPER_ID = new Identifier("minecraft","entities/creeper");
+    private static final Identifier SUSPICIOUS_SAND_ID = new Identifier("minecraft", "archaeology/desert_pyramid");
+
     public static void modifyLootTables(){
-        LootTableEvents.MODIFY.register(((resourceManager, lootManager, id, tableBuilder, source) -> {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             if(JUNGLE_TEMPLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
@@ -35,6 +44,15 @@ public class ModLootTablesModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
-        }));
+        });
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if(SUSPICIOUS_SAND_ID.equals(id)){
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                LootPool.Builder pool = LootPool.builder().with(entries);
+                return LootTable.builder().pool(pool).build();
+            }
+            return null;
+        });
     }
 }
