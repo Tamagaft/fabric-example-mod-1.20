36c0c140f5679357551772d6af144e91ed2b7939
Tamagaft <ihorvoloshanovich@gmail.com>
2024-02-14 17:07:08 +0200

    added Porcupine attack

--- src/main/java/net/tamagaft/testmod/entity/custom/PorcupineEntity.java
+++ src/main/java/net/tamagaft/testmod/entity/custom/PorcupineEntity.java
@@ -7,6 +7,9 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
@@ -17,12 +20,18 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 import net.tamagaft.testmod.entity.ModEntities;
+import net.tamagaft.testmod.entity.ai.PorcupineAttackGoal;
 import net.tamagaft.testmod.item.ModItems;
 import org.jetbrains.annotations.Nullable;
 
 public class PorcupineEntity extends AnimalEntity {
+
+    private static final TrackedData<Boolean> ATTACKING =
+            DataTracker.registerData(PorcupineEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+    public final AnimationState attackAnimationState = new AnimationState();
+    public int attackAnimationTimeout = 0;
     public PorcupineEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -33,6 +42,17 @@ public class PorcupineEntity extends AnimalEntity {
             this.idleAnimationState.start(this.age);
         } else {
             --this.idleAnimationTimeout;
+        }
+
+        if(this.isAttacking() && attackAnimationTimeout <= 0){
+            attackAnimationTimeout = 40;
+            attackAnimationState.start(this.age);
+        }else {
+            --this.attackAnimationTimeout;
+        }
+
+        if (!this.isAttacking()){
+            attackAnimationState.stop();
         }
     }
 
@@ -53,11 +73,14 @@ public class PorcupineEntity extends AnimalEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1,new PorcupineAttackGoal(this, 1D, true));
         this.goalSelector.add(1,new AnimalMateGoal(this, 10));
         this.goalSelector.add(2,new TemptGoal(this, 1.25, Ingredient.ofItems(ModItems.TOMATO),false));
         this.goalSelector.add(3, new WanderAroundFarGoal(this,1D));
         this.goalSelector.add(4,new FollowParentGoal(this, 1.2));
         this.goalSelector.add(5,new LookAroundGoal(this));
+
+        this.targetSelector.add(1, new RevengeGoal(this));
 
     }
 
@@ -68,6 +91,21 @@ public class PorcupineEntity extends AnimalEntity {
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3f)
                 .add(EntityAttributes.GENERIC_ARMOR, 2f)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE,3);
+    }
+
+    public void setAttacking(boolean attacking){
+        this.dataTracker.set(ATTACKING, attacking);
+    }
+
+    @Override
+    public boolean isAttacking() {
+        return this.dataTracker.get(ATTACKING);
+    }
+
+    @Override
+    protected void initDataTracker(){
+        super.initDataTracker();
+        this.dataTracker.startTracking(ATTACKING,false);
     }
 
     @Override
